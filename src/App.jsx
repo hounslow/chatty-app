c320af5270c0c14eea0c4536ffafec1627f7ac10
hounslow <hounslowmatthew@gmail.com>
2017-08-22 20:13:51 +0000

    Changed the files to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
 
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+      <div>
+        <div>
+          <MessageList />
+        </div>
+        <div>
+          <ChatBar />
+        </div>
+      </div>
+    </div>
     );
   }
 }
